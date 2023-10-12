c607bcd1ba433259abda8f9f94e30f70310dccd2
dhubleur <dhubleur@student.42.fr>
2023-10-12 15:32:25 +0200

    design

--- src/main/java/com/codingame/game/Player.java
+++ src/main/java/com/codingame/game/Player.java
@@ -1,9 +1,18 @@
 package com.codingame.game;
 import com.codingame.gameengine.core.AbstractMultiplayerPlayer;
+import com.codingame.gameengine.module.entities.Group;
 
 public class Player extends AbstractMultiplayerPlayer {
+	public Group hud;
+
+	private int credits = 0;
+	
     @Override
     public int getExpectedOutputLines() {
         return 1;
     }
+
+	public int getCredits() {
+		return credits;
+	}
 }
--- src/main/java/com/codingame/game/Referee.java
+++ src/main/java/com/codingame/game/Referee.java
@@ -7,6 +7,10 @@ import com.codingame.gameengine.core.AbstractReferee;
 import com.codingame.gameengine.core.GameManager;
 import com.codingame.gameengine.core.MultiplayerGameManager;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.codingame.gameengine.module.entities.Group;
+import com.codingame.gameengine.module.entities.Line;
+import com.codingame.gameengine.module.entities.Sprite;
+import com.codingame.gameengine.module.entities.Text;
 import com.google.inject.Inject;
 
 public class Referee extends AbstractReferee {
@@ -20,12 +24,77 @@ public class Referee extends AbstractReferee {
 		random = new Random(gameManager.getSeed());
 
         drawBackground();
+		drawHud();
+		drawGrid();
     }
 
 	private void drawBackground() {
 		graphicEntityModule.createSprite()
 			.setImage("Background.jpg")
 			.setAnchor(0);
+	}
+
+	private void drawHud() {
+        for (Player player : gameManager.getPlayers()) {
+            int x = player.getIndex() == 0 ? 180 : 1920 - 180;
+            int y = 120;
+
+            graphicEntityModule
+                    .createRectangle()
+                    .setWidth(140)
+                    .setHeight(140)
+                    .setX(x - 70)
+                    .setY(y - 70)
+                    .setLineWidth(0)
+                    .setFillColor(player.getColorToken());
+
+            graphicEntityModule
+                    .createRectangle()
+                    .setWidth(120)
+                    .setHeight(120)
+                    .setX(x - 60)
+                    .setY(y - 60)
+                    .setLineWidth(0)
+                    .setFillColor(0xffffff);
+
+            Text name = graphicEntityModule.createText(player.getNicknameToken())
+                    .setX(x)
+                    .setY(y + 120)
+                    .setZIndex(20)
+                    .setFontSize(40)
+                    .setFillColor(0xffffff)
+                    .setAnchor(0.5);
+
+			Text credit = graphicEntityModule.createText("Credit: " + player.getCredits())
+                    .setX(x)
+                    .setY(y + 170)
+                    .setZIndex(20)
+                    .setFontSize(40)
+                    .setFillColor(0xffffff)
+                    .setAnchor(0.5);
+
+            Sprite avatar = graphicEntityModule.createSprite()
+                    .setX(x)
+                    .setY(y)
+                    .setZIndex(20)
+                    .setImage(player.getAvatarToken())
+                    .setAnchor(0.5)
+                    .setBaseHeight(116)
+                    .setBaseWidth(116);
+
+            player.hud = graphicEntityModule.createGroup(name, credit, avatar);
+        }
+    }
+
+
+	private void drawGrid() {
+		graphicEntityModule
+			.createSprite()
+			.setImage("board_border.png")
+			.setX(1920 / 2)
+			.setY(1080 / 2)
+			.setScale(1.2)
+			.setAnchor(0.5);
 	}
 
     @Override

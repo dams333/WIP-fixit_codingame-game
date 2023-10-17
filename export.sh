7fd6623aa493bec78adf82f9226fcfe80c3d992c
dhubleur <dhubleur@student.42.fr>
2023-10-17 15:34:17 +0200

    boss

--- export.sh
+++ export.sh
@@ -1,8 +1,11 @@
+rm -f export.zip
 mkdir -p export
 cp -r src/ export/src
 cp pom.xml export/pom.xml
 sed '/<build>/,/<\/build>/d' export/pom.xml > export/pom.xml.tmp
 mv export/pom.xml.tmp export/pom.xml
 cp -r config/ export/config
-zip -r export.zip export
+cd export
+zip -r ../export.zip *
+cd ..
 rm -rf export

3169be68b1f76ac69d4c30dd8dbaa9140c6da932
dhubleur <dhubleur@student.42.fr>
2023-10-20 13:16:26 +0200

    boss

--- config/Boss.java
+++ config/Boss.java
@@ -15,30 +15,55 @@ class Player {
 
         // game loop
         while (true) {
+            List<List<Integer>> grid;
+            grid = new ArrayList<List<Integer>>(mapWidth);
+            for (int i = 0; i < mapWidth; i++) {
+                grid.add(new ArrayList<Integer>(mapHeight));
+            }
             int caseCount = in.nextInt();
+            int bugMax = 0;
             for (int i = 0; i < caseCount; i++) {
                 int x = in.nextInt();
                 int y = in.nextInt();
                 int bugLevel = in.nextInt(); // 0 = no bug / 1,2,3 = bug level on the case
+                grid.get(x).add(bugLevel);
+                if (bugLevel > bugMax)
+                    bugMax = bugLevel;
             }
             int fixerCount = in.nextInt();
 			int myFixerId = -1;
+            int myFixerX = -1;
+            int myFixerY = -1;
             for (int i = 0; i < fixerCount; i++) {
                 int id = in.nextInt();
                 int x = in.nextInt();
                 int y = in.nextInt();
                 int owner = in.nextInt(); // 1 = you / -1 = opponent
-				if (owner == 1)
+				if (owner == 1) {
 					myFixerId = id;
+                    myFixerX = x;
+                    myFixerY = y;
+                }
             }
 
             // Write an answer using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
-			Random rand = new Random();
-			int x = rand.nextInt(mapWidth);
-			int y = rand.nextInt(mapHeight);
-            System.out.println("MOVE " + myFixerId + " " + x + " " + y); // MOVE <id> <x> <y>
+            if (grid.get(myFixerX).get(myFixerY) > 0) {
+                System.out.println("WAIT");
+            } else {
+				int x = 0;
+            	int y = 0;
+                for (int i = 0; i < mapWidth; i++) {
+                    for (int j = 0; j < mapHeight; j++) {
+                        if (grid.get(i).get(j) == bugMax) {
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+				System.out.println("MOVE " + myFixerId + " " + x + " " + y); // MOVE <id> <x> <y>
+            }
         }
     }
 }
